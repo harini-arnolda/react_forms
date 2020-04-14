3b141d99260e29006c958c2c6db3834e93bea11b
Harini Arnolda <HariniArnolda@Widuras-MacBook-Pro.local>
2020-04-14 13:37:38 +0530

    Add styles to the search results

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,6 @@ class App extends React.Component {
     state = { images: [] };
     onSearchSubmit = async term => {
 
-        console.log("inside on searchsubmit");
-
         /** To make a get request via axios we need to install axios then need 
          * to call the axios.get fuction, It tekes two parameters the first one is the url 
          * the secodn argument is the pare parameters to configure the request*/
